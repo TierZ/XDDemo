1bffe6400cd3986efd5db40977f53aa773430191
左晓东 <zuoxiaodong1989@126.com>
2018-10-19 16:45:45 +0800

    Just for fun

---
+++ ForPlayDemo/ForPlayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForPlayDemo
+//
+//  Created by 左晓东 on 2018/10/19.
+//  Copyright © 2018年 左晓东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ForPlayDemo/ForPlayDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ForPlayDemo
+//
+//  Created by 左晓东 on 2018/10/19.
+//  Copyright © 2018年 左晓东. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"just for fun");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
